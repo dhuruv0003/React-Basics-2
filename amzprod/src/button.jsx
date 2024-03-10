01cbdec0e3bf3e9964d2a0d6388d39d4c3ad9fd6
dhuruv0003 <116896454+dhuruv0003@users.noreply.github.com>
2024-03-11 00:49:07 +0530

    "new commit"

--- amzprod/src/button.jsx
+++ amzprod/src/button.jsx
@@ -1,15 +1,19 @@
-function dosomething()
+function handleclick()
 {
     console.log("button was clicked");
 }
-function donothing(){
-    console.log("no para was clocked");
+function handleover(){
+    console.log("no para was cloiked");
+}
+function handledblclick(){
+    console.log("you clicked twice");
 }
 export default function Button(){   
     return (
         <div>
-            <button onClick={dosomething}>Click me!!</button>
-            <p onClick={donothing}>Hello</p>
+            <button onClick={handleclick}>Click me!!</button>
+            <p onMouseOver={handleover}>Hello Lorem ipsum dolor, sit amet consectetur adipisicing elit. Alias unde quis architecto error laudantium ratione maxime voluptates ad, est ducimus incidunt deleniti cupiditate ipsum veritatis provident tempora, perferendis fugit non!</p>
+            <h1 onDoubleClick={handledblclick}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam molestiae debitis, nulla, numquam accusantium pariatur soluta ipsum unde expedita minima, voluptates incidunt. Cumque praesentium ullam explicabo reprehenderit eligendi itaque.</h1>
         </div>
     );
 }
